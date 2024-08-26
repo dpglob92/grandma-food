a7892c825c2ac5781c267b48ef2e4b51ee7cc84d
darwin.granados.martinez <darwin.granados@globant.com>
2024-08-26 13:55:17 -0500

    perf: improve findByDocumentId method

--- src/main/java/com/javastudio/grandmafood/features/core/database/repositories/ClientJPAEntityRepository.java
+++ src/main/java/com/javastudio/grandmafood/features/core/database/repositories/ClientJPAEntityRepository.java
@@ -2,6 +2,7 @@ package com.javastudio.grandmafood.features.core.database.repositories;
 
 import com.javastudio.grandmafood.features.core.database.entities.ClientJPAEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +11,6 @@ import java.util.UUID;
 @Repository
 public interface ClientJPAEntityRepository extends JpaRepository<ClientJPAEntity, UUID> {
 
+    @Query("SELECT u FROM client u WHERE u.documentId = ?1 AND u.deletedAt IS NULL")
     Optional<ClientJPAEntity> findByDocumentId(String documentId);
 }
