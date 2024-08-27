f54a92a0c66b5ede0486533bf44aaf7529f4528c
DeadEnd777 <darwingranados96@gmail.com>
2024-08-27 15:45:21 -0500

    fix: delete validator to ClientFindUseCase method

--- src/main/java/com/javastudio/grandmafood/features/core/usecases/ClientFindUseCase.java
+++ src/main/java/com/javastudio/grandmafood/features/core/usecases/ClientFindUseCase.java
@@ -5,8 +5,6 @@ import com.javastudio.grandmafood.features.core.database.entities.ClientJPAEntit
 import com.javastudio.grandmafood.features.core.database.repositories.ClientJPAEntityRepository;
 import com.javastudio.grandmafood.features.core.definitions.client.IClientFindUseCase;
 import com.javastudio.grandmafood.features.core.entities.client.Client;
-import com.javastudio.grandmafood.features.errors.ClientNotFoundException;
-import jakarta.validation.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -19,34 +17,22 @@ public class ClientFindUseCase implements IClientFindUseCase {
     Logger logger = LoggerFactory.getLogger(ClientFindUseCase.class);
 
     private final ClientJPAEntityRepository repository;
-    private final Validator validator;
 
-    public ClientFindUseCase(ClientJPAEntityRepository repository, Validator validator) {
+    public ClientFindUseCase(ClientJPAEntityRepository repository) {
         this.repository = repository;
-        this.validator = validator;
     }
 
     @Override
     public Optional<Client> findById(UUID uuid) {
-        validator.validate(uuid);
-
         logger.info("Finding client with id: {}", uuid);
         Optional<ClientJPAEntity> clientOptional = repository.findById(uuid);
-        if (clientOptional.isEmpty()) {
-            throw new ClientNotFoundException();
-        }
         return clientOptional.map(ClientAdapter::jpaEntityToDomain);
     }
 
     @Override
     public Optional<Client> findByDocument(String documentId) {
-        validator.validate(documentId);
-
         logger.info("Finding client with documentId: {}", documentId);
         Optional<ClientJPAEntity> clientOptional = repository.findByDocumentId(documentId);
-        if (clientOptional.isEmpty()) {
-            throw new ClientNotFoundException();
-        }
         return clientOptional.map(ClientAdapter::jpaEntityToDomain);
     }
 }
