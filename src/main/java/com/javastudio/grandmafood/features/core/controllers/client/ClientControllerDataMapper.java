c5c3eee29778422bc67bf7c687403e491910e38b
DeadEnd777 <darwingranados96@gmail.com>
2024-08-28 12:37:39 -0500

    feat: add adapter of client

---
+++ src/main/java/com/javastudio/grandmafood/features/core/controllers/client/ClientControllerDataMapper.java
@@ -0,0 +1,31 @@
+package com.javastudio.grandmafood.features.core.controllers.client;
+
+import com.javastudio.grandmafood.features.core.entities.client.Client;
+import com.javastudio.grandmafood.features.core.entities.client.ClientCreateInput;
+
+public class ClientControllerDataMapper {
+    public static ClientCreateInput clientToDomain(ClientCreateDTO data) {
+        ClientDocumentUtils.DocumentData documentData = ClientDocumentUtils.separateDocument(data.getDocument());
+        return ClientCreateInput.builder()
+                .documentType(documentData.documentType())
+                .documentId(documentData.documentId())
+                .name(data.getName())
+                .phone(data.getPhone())
+                .email(data.getEmail())
+                .deliveryAddress(data.getDeliveryAddress())
+                .build();
+    }
+
+    public static ClientResponseModel clientResponseModel(Client client) {
+        String document = ClientDocumentUtils.concatenateDocument(client.getDocumentType(), client.getDocumentId());
+        return ClientResponseModel.builder()
+                .document(document)
+                .name(client.getName())
+                .email(client.getEmail())
+                .phone(client.getPhone())
+                .deliveryAddress(client.getDeliveryAddress())
+                .build();
+    }
+
+
+}
