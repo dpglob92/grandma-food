4328c0b66b8bc8c2f8436ab4024294aef7df4863
DeadEnd777 <darwingranados96@gmail.com>
2024-08-30 13:17:59 -0500

    feat: add delete product method

--- src/main/java/com/javastudio/grandmafood/features/core/controllers/product/ProductController.java
+++ src/main/java/com/javastudio/grandmafood/features/core/controllers/product/ProductController.java
@@ -7,6 +7,7 @@ import com.javastudio.grandmafood.features.core.controllers.product.dto.ProductD
 import com.javastudio.grandmafood.features.core.entities.product.Product;
 import com.javastudio.grandmafood.features.core.entities.product.ProductCreateInput;
 import com.javastudio.grandmafood.features.core.usecases.product.ProductCreateUseCase;
+import com.javastudio.grandmafood.features.core.usecases.product.ProductDeleteUseCase;
 import com.javastudio.grandmafood.features.core.usecases.product.ProductFindUseCase;
 import com.javastudio.grandmafood.features.errors.ProductNotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,6 +16,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,15 +31,18 @@ public class ProductController {
     private final ProductCreateUseCase createUseCase;
     private final ProductFindUseCase findUseCase;
     private final ProductDTOMapper productDTOMapper;
+    private final ProductDeleteUseCase deleteUseCase;
 
     public ProductController(
             ProductCreateUseCase createUseCase,
             ProductFindUseCase findUseCase,
-            ProductDTOMapper productDTOMapper
+            ProductDTOMapper productDTOMapper,
+            ProductDeleteUseCase deleteUseCase
     ) {
         this.createUseCase = createUseCase;
         this.findUseCase = findUseCase;
         this.productDTOMapper = productDTOMapper;
+        this.deleteUseCase = deleteUseCase;
     }
 
     @PostMapping("/")
@@ -100,6 +105,31 @@ public class ProductController {
         Optional<Product> product = findUseCase.findById(parsedUuid);
         ProductDTO productDTO = productDTOMapper.domainToDTO(product.orElseThrow(ProductNotFoundException::new));
         return ResponseEntity.status(201).body(productDTO);
+    }
+
+    @DeleteMapping("/{uuid}")
+    @Operation(summary = "Delete a product by uuid")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Product not found",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid uuid",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    }
+            ),
+    })
+    ResponseEntity<Void> deleteOne(@PathVariable("uuid") String uuid) {
+        UUID parsedUuid = ValidationUtils.parseUUID(uuid);
+        deleteUseCase.deleteById(parsedUuid);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
---
+++ src/test/java/com/javastudio/grandmafood/core/integration/product/ProductDeleteIntegrationTests.java
@@ -0,0 +1,77 @@
+package com.javastudio.grandmafood.core.integration.product;
+
+import com.javastudio.grandmafood.common.exceptions.ExceptionCode;
+import com.javastudio.grandmafood.common.exceptions.InvalidInputException;
+import com.javastudio.grandmafood.common.exceptions.ValidationUtils;
+import com.javastudio.grandmafood.core.utils.ProductTestUtil;
+import com.javastudio.grandmafood.features.core.entities.product.Product;
+import com.javastudio.grandmafood.features.core.usecases.product.ProductCreateUseCase;
+import com.javastudio.grandmafood.features.core.usecases.product.ProductDeleteUseCase;
+import com.javastudio.grandmafood.features.core.usecases.product.ProductFindUseCase;
+import com.javastudio.grandmafood.features.errors.ProductNotFoundException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.UUID;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ProductDeleteIntegrationTests {
+
+    @Autowired
+    private ProductDeleteUseCase productDeleteUseCase;
+
+    @Autowired
+    private ProductCreateUseCase productCreateUseCase;
+
+    @Autowired
+    private ProductFindUseCase productFindUseCase;
+
+    private Product product;
+
+    @BeforeEach
+    public void setUp() {
+        var validInput1 = ProductTestUtil.getValidProductCreateInput();
+        this.product = productCreateUseCase.create(validInput1);
+
+        var validInput2 = ProductTestUtil.getValidProductCreateInput().toBuilder()
+                .name("CHEESE BURGER BIG COMBO")
+                .build();
+        productCreateUseCase.create(validInput2);
+    }
+
+    @Test
+    public void Should_DeleteProduct_GivenProductId() {
+
+        productDeleteUseCase.deleteById(product.getUuid());
+
+        var productFound = productFindUseCase.findById(product.getUuid());
+
+        Assertions.assertThat(productFound).isEmpty();
+    }
+
+    @Test
+    public void Should_ThrowNotFoundException_WhenProductIsNotFound() {
+
+        Assertions.assertThatThrownBy(() -> productDeleteUseCase.deleteById(UUID.randomUUID()))
+                .isInstanceOf(ProductNotFoundException.class)
+                .extracting("exceptionCode")
+                .isEqualTo(ExceptionCode.NOT_FOUND);
+    }
+
+    @Test
+    public void Should_ThrowInvalidInputException_WhenUuidIsInvalid() {
+        String invalidUUID = "invalid-uuid-string";
+        Assertions.assertThatThrownBy(() -> {
+            UUID uuid = ValidationUtils.parseUUID(invalidUUID);
+            productDeleteUseCase.deleteById(uuid);
+                })
+                .isInstanceOf(InvalidInputException.class)
+                .extracting("exceptionCode")
+                .isEqualTo(ExceptionCode.INVALID_INPUT);
+    }
+}
