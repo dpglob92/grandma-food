f074dbd6a0e0fc04fc06ea7a462515c0aec34a0b
dpglob92 <diego.puche@globant.com>
2024-09-05 22:12:40 -0500

    test: add tests for OrderInfoUseCase

--- src/main/java/com/javastudio/grandmafood/features/core/usecases/orders/OrderInfoUseCase.java
+++ src/main/java/com/javastudio/grandmafood/features/core/usecases/orders/OrderInfoUseCase.java
@@ -3,11 +3,21 @@ package com.javastudio.grandmafood.features.core.usecases.orders;
 import com.javastudio.grandmafood.features.core.definitions.orders.IOrderInfoUseCase;
 import com.javastudio.grandmafood.features.core.entities.orders.Order;
 import com.javastudio.grandmafood.features.core.entities.orders.OrderInfo;
+import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
+@Component
 public class OrderInfoUseCase implements IOrderInfoUseCase {
+
+    public static final BigDecimal ivaAsPercentage = new BigDecimal("0.19");
 
     @Override
     public OrderInfo computeOrderInfo(Order order) {
-        return null;
+        return new OrderInfo(
+                new BigDecimal("0"),
+                new BigDecimal("0"),
+                new BigDecimal("0")
+        );
     }
 }
